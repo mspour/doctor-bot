04bd90c7cef1c9ef29a9ad1bf798f7cccd4e5d3e
mspour <mspour02@gmail.com>
2026-08-06 15:25:12 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# تنظیمات ربات نوبت پزشک
+
+# فاصله بررسی بر حسب ثانیه
+CHECK_INTERVAL = 600   # هر 10 دقیقه
+
+# اطلاعات تلگرام
+TELEGRAM_BOT_TOKEN = "YOUR_BOT_TOKEN"
+TELEGRAM_CHAT_ID = "YOUR_CHAT_ID"
+
+
+# لیست پزشکان
+DOCTORS = [
+    {
+        "name": "دکتر سمیه جفرسته",
+        "id": "1042"
+    }
+]
